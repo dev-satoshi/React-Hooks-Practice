552cb7d67c30077a611052f51486999c33aaf746
satoshi <dev.satoshi.1016@gmail.com>
2023-06-25 07:26:02 +0900

    add: useEffect実践

--- react-hooks-practice/src/App.tsx
+++ react-hooks-practice/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import Counter from "./components/counter/Counter";
 import InputForm from "./components/InputForm";
+import ItemList from "./components/itemList/ItemList";
 
 function App() {
   const [isDisplay, setIsDisplay] = useState(true);
@@ -11,11 +12,12 @@ function App() {
   return (
     <div className="App">
       <h2>useEffect</h2>
-      <button onClick={() => handleDisplay()}>
+      {/* <button onClick={() => handleDisplay()}>
         {isDisplay ? "コンポーネント非表示" : "コンポーネント表示"}
-      </button>
-      {isDisplay && <Counter />}
+      </button> */}
+      {/* {isDisplay && <Counter />} */}
       {/* <InputForm /> */}
+      <ItemList />
     </div>
   );
 }
